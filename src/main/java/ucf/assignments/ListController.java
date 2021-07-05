51f0ae3b7d3eac1d8ff6db1cff2e25e41556a90a
cristiamenciso <cristiam.enciso@gmail.com>
2021-07-04 23:27:22 -0400

    Assignment 4 part 1

---
+++ src/main/java/ucf/assignments/ListController.java
@@ -0,0 +1,35 @@
+package ucf.assignments;
+
+import javafx.event.ActionEvent;
+
+public class ListController {
+    public void selectListButtonClicked(ActionEvent actionEvent) {
+    }
+
+    public void addListButtonClicked(ActionEvent actionEvent) {
+    }
+
+    public void removeListButtonClicked(ActionEvent actionEvent) {
+    }
+
+    public void addItemButtonClicked(ActionEvent actionEvent) {
+    }
+
+    public void saveButtonClicked(ActionEvent actionEvent) {
+    }
+
+    public void loadButtonClicked(ActionEvent actionEvent) {
+    }
+
+    public void removeItemButtonClicked(ActionEvent actionEvent) {
+    }
+
+    public void editItemButtonClicked(ActionEvent actionEvent) {
+    }
+
+    public void editDateButtonClciked(ActionEvent actionEvent) {
+    }
+
+    public void markCompleteButtonClicked(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/ucf/assignments/ListManager.java
+++ src/main/java/ucf/assignments/ListManager.java
@@ -1,7 +1,11 @@
 package ucf.assignments;
-
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ListManager extends Application {
 
@@ -11,6 +15,17 @@ public class ListManager extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("ListManager.fxml"));
+
+            Scene scene = new Scene(root);
+
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("ListManager");
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
